35f16141425dce7da662f4ab04e34e936126704a
вадим никанович <nikanovich8228@mail.ru>
2023-05-08 23:10:27 +0300

    upd

---
+++ Drandulette/APIEssentials/Controllers/GlobalMethods.cs
@@ -0,0 +1,33 @@
+﻿using Drandulette.Controllers.Data.Models;
+using FileManager = System.IO.File;
+
+namespace Drandulette.APIEssentials.Controllers
+{
+    public static class GlobalMethods
+    {
+        public static Announcement InsertPictures(Announcement x, int isSpecific)
+        {
+            var pics = Directory.EnumerateFiles(x.picsPath);
+
+            if (isSpecific == 0)
+            {
+                x.pics = pics.Select(x => new StreamReader(x).ReadToEnd()).ToList();
+                return x;
+            }
+
+            x.pics.Add(pics.First());
+            return x;
+        }
+
+        public static Topic InsertPictures(Topic x)
+        {
+            x.user.profilePic = Convert.ToBase64String(FileManager.ReadAllBytes($".\\Users\\{x.mailLogin}\\imgnotfound.jpg"))
+            return x;
+        }
+
+        public static bool Matches(string brand, string model, int year, Announcement x)
+        {
+            return x.brand.Contains(brand) || x.model.Contains(model) || x.year == year;
+        }
+    }
+}
